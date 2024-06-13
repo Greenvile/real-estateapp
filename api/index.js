c3644b2d79a0df9dcaf2e55046a18260c27f3ba9
jonus green <jonusashaba@gmail.com.com>
2024-06-13 12:23:18 +0300

    only commit

--- api/index.js
+++ api/index.js
@@ -6,32 +6,43 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+
+// Load environment variables from .env file
 dotenv.config();
 
+// Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log("connected to MongoDB");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Failed to connect to MongoDB", err);
   });
 
-const __dirname = path.resolve();
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
+// API Routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// Serve static assets if in production. Must come after routes.
-app.use(express.static(path.join(__dirname, "client")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
